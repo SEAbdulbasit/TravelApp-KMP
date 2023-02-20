aac6eae760b9ded79a430f138229986ec9f1ffbb
Abdul Basit <abdulbasit5361234@gmail.com>
2023-02-20 08:46:00 +0500

    Slide image size issue fixed

--- shared/src/jsMain/kotlin/main.js.kt
+++ shared/src/jsMain/kotlin/main.js.kt
@@ -1,30 +1,7 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import com.example.traveapp_kmp.CommonView
-import com.seiko.imageloader.ImageLoader
-import com.seiko.imageloader.LocalImageLoader
-import com.seiko.imageloader.cache.memory.maxSizePercent
-import com.seiko.imageloader.component.setupDefaultComponents
-import com.seiko.imageloader.util.DebugLogger
-import com.seiko.imageloader.util.LogPriority
-
 
 @Composable
-fun MainViewWeb() {
-//    CompositionLocalProvider(
-//        LocalImageLoader provides ImageLoader {
-//            components {
-//                setupDefaultComponents(imageScope)
-//            }
-//            interceptor {
-//                memoryCacheConfig {
-//                    maxSizePercent(0.25)
-//                }
-//            }
-//        },
-//    ) {
-        CommonView(400.toFloat())
- //   }
-}
+fun MainView() = CommonView(size.width)
 
 
