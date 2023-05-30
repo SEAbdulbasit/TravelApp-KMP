59dfdf9606853f4601bced71012b00c0d298bebb
Abdul Basit <abdulbasit5361234@gmail.com>
2023-05-30 22:14:39 +0500

    Add web app

--- desktopApp/src/jvmMain/kotlin/example/travelapp_kmp/Main.kt
+++ desktopApp/src/jvmMain/kotlin/example/travelapp_kmp/Main.kt
@@ -1,15 +1,13 @@
 package example.travelapp_kmp
 
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import com.example.travelapp_kmp.AppViewDesktop
-import java.awt.Dimension
-import java.awt.Toolkit
 
 fun main() = application {
     Window(
@@ -17,19 +15,9 @@ fun main() = application {
         title = "TravelApp-KMP",
         state = WindowState(
             position = WindowPosition.Aligned(Alignment.Center),
-            size = getPreferredWindowSize(1000, 1000)
+            size = DpSize(Dp(1000f), Dp(1500f))
         ),
     ) {
         AppViewDesktop()
     }
 }
-
-fun getPreferredWindowSize(desiredWidth: Int, desiredHeight: Int): DpSize {
-    val screenSize: Dimension = Toolkit.getDefaultToolkit().screenSize
-    val preferredWidth: Int = (screenSize.width * 0.8f).toInt()
-    val preferredHeight: Int = (screenSize.height * 0.8f).toInt()
-    val width: Int = if (desiredWidth < preferredWidth) desiredWidth else preferredWidth
-    val height: Int = if (desiredHeight < preferredHeight) desiredHeight else preferredHeight
-    return DpSize(width.dp, height.dp)
-}
-
