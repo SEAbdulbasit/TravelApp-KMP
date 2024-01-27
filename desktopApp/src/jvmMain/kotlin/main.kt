386a9af01b0a85c079b07bf23b08396dafd89109
Abdul Basit <abdulbasit5361234@gmail.com>
2024-01-27 14:35:25 +0500

    - Fix desktop

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,14 +1,9 @@
-package example.travelapp_kmp
-
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.example.travelapp_kmp.AppViewDesktop
 
 fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "TravelApp-KMP"
-    ) {
+    Window(title = "TravelApp", onCloseRequest = ::exitApplication) {
         AppViewDesktop()
     }
 }
