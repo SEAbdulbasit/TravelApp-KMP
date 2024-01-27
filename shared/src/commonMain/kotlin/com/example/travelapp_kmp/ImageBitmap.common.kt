1124d4a2138fbcc153ad9b7080c4e03af2d45be5
Abdul Basit <abdulbasit5361234@gmail.com>
2024-01-27 13:20:45 +0500

    - Add image bitmap implementation

--- shared/src/commonMain/kotlin/com/example/travelapp_kmp/ImageBitmap.common.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travelapp_kmp
-
-import androidx.compose.ui.graphics.ImageBitmap
-
-expect fun ByteArray.toImageBitmap(): ImageBitmap
--- shared/src/iosMain/kotlin/com/example/travelapp_kmp/ImageBitmap.common.ios.kt
+++
@@ -1,8 +0,0 @@
-package com.example.travelapp_kmp
-
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toComposeImageBitmap
-import org.jetbrains.skia.Image
-
-actual fun ByteArray.toImageBitmap(): ImageBitmap =
-    Image.makeFromEncoded(this).toComposeImageBitmap()
