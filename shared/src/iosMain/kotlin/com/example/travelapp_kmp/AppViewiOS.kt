9a3f01534fb68c80af48fcdcec927c447b940886
Abdul Basit <abdulbasit5361234@gmail.com>
2024-01-27 13:01:46 +0500

    - Remove the box from views

--- shared/src/iosMain/kotlin/com/example/travelapp_kmp/AppViewiOS.kt
+++ shared/src/iosMain/kotlin/com/example/travelapp_kmp/AppViewiOS.kt
@@ -1,43 +1,8 @@
 package com.example.travelapp_kmp
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.toSize
-import com.example.travelapp_kmp.CommonView
-import com.seiko.imageloader.ImageLoader
-import com.seiko.imageloader.LocalImageLoader
-import com.seiko.imageloader.cache.memory.maxSizePercent
-import com.seiko.imageloader.component.setupDefaultComponents
-import com.seiko.imageloader.util.DebugLogger
-import com.seiko.imageloader.util.LogPriority
 
 @Composable
 internal fun AppViewiOS() {
-    val size = remember { mutableStateOf(IntSize.Zero) }
-    Box(Modifier.fillMaxSize().onGloballyPositioned { coordinates ->
-        size.value = coordinates.size
-    }) {
-        CompositionLocalProvider(
-            LocalImageLoader provides ImageLoader {
-                logger = DebugLogger(LogPriority.VERBOSE)
-                components {
-                    setupDefaultComponents(imageScope)
-                }
-                interceptor {
-                    memoryCacheConfig {
-                        maxSizePercent(0.25)
-                    }
-                }
-            },
-        ) {
-            CommonView()
-        }
-    }
+    CommonView()
 }
